cc2b97b607bbc463d627e9d4a7fd6a54f6296f7a
Ben Womble <69554103+BenWomble@users.noreply.github.com>
2022-12-10 19:44:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import discord
 from discord.ext import commands
 
-TOKEN = os.getenv('DISCORD_TOKEN')
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
